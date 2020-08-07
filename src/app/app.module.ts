9c10175459833d9252bd7daeabcb745fd51e92a3
Yossi Even <yossi.even@gmail.com>
2020-08-08 00:07:15 +0300

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+import { NotepadComponent } from './notepad/notepad.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent,
+    NotepadComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule
