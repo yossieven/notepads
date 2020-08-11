98f347c65083675510d4561a8f33eba1e0f575f8
Yossi Even <yossi.even@gmail.com>
2020-08-11 22:08:19 +0300

    board of notepads

---
+++ src/app/Notepad.ts
@@ -0,0 +1,6 @@
+export class Notepad{
+  id: string;
+  name: string;
+  color: string;
+  background: string;
+}
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Apollo, gql} from 'apollo-angular';
+import {Notepad} from '../Notepad';
 
 @Component({
   selector: 'app-board',
@@ -8,26 +9,35 @@ import {Apollo, gql} from 'apollo-angular';
 })
 export class BoardComponent implements OnInit {
   notes: any[];
+  notepads: Array<Notepad> = [];
 
   constructor(private apollo: Apollo) { }
 
   ngOnInit(): void {
-      this.apollo
+    this.apollo
       .watchQuery({
         query: gql`
           {
-            notes {
-              title
-              text
+            notepads {
+              id
+              name
+              color
+              background
             }
           }
         `,
       })
       .valueChanges.subscribe(({ data, loading }) => {
-      console.log('data ', data);
+      // @ts-ignore
+      for (const notepad of data.notepads){
+        this.notepads.push(notepad);
+      }
       console.log('loading ', loading);
     });
   }
 
+  addNotepad(){
+
+  }
 
 }
